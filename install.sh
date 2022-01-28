9b7652f24e7dccea62410525c02af236e9282316
ririxi <mail@ririxi.dev>
2022-01-28 19:55:37 +0100

    chore: switch to my repo

--- install.sh
+++ install.sh
@@ -6,15 +6,14 @@ firstpath=/absolute/path/to/repo
 secondpath=/absolute/path/to/node
 echo "[INFO] Running script"
 echo "[+] Replacing paths in plist file..."
-sed -i '' "s|$firstpath|$dir|g" sleequid.am-rpc.plist
-sed -i '' "s|$secondpath|$nodebin|g" sleequid.am-rpc.plist
+sed -i '' "s|$firstpath|$dir|g" ririxidev.am-rpc.plist
+sed -i '' "s|$secondpath|$nodebin|g" ririxidev.am-rpc.plist
 echo "[+] Copying plist file to ~/Library/LaunchAgents"
-cp sleequid.am-rpc.plist ~/Library/LaunchAgents/
+cp ririxidev.am-rpc.plist ~/Library/LaunchAgents/
 echo "[+] Starting daemon..."
-launchctl load ~/Library/LaunchAgents/sleequid.am-rpc.plist
-echo "[+] Started daemon... Check your discord status!"
+launchctl load ~/Library/LaunchAgents/ririxidev.am-rpc.plist
+echo "[+] Started daemon..."
 echo "[+] Daemon will start every-time you login."
-echo "[+] To stop daemon, run: sudo launchctl unload ~/Library/LaunchAgents/sleequid.am-rpc.plist"
-echo "[+] To start daemon, run: sudo launchctl load ~/Library/LaunchAgents/sleequid.am-rpc.plist"
-echo "[+] To remove daemon, run: sudo launchctl unload ~/Library/LaunchAgents/sleequid.am-rpc.plist && rm ~/Library/LaunchAgents/sleequid.am-rpc.plist"
-echo "[+] To check daemon logs, run: sudo tail -f /var/log/syslog"
+echo "[+] To stop daemon, run: sudo launchctl unload ~/Library/LaunchAgents/ririxidev.am-rpc.plist"
+echo "[+] To start daemon, run: sudo launchctl load ~/Library/LaunchAgents/ririxidev.am-rpc.plist"
+echo "[+] To remove daemon, run: sudo launchctl unload ~/Library/LaunchAgents/ririxidev.am-rpc.plist && rm ~/Library/LaunchAgents/ririxidev.am-rpc.plist"
